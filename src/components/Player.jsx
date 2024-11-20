14006e219e91e9f26a1ff44c4778187ddc0a595a
Andrew <andrewchau1993@gmail.com>
2024-11-20 15:50:58 -0800

    useState now accepts input elements

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,31 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
-  // let btnCaption = 'Edit';
+  function handlePlayerNameChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" value={name} required/>
-    // btnCaption = 'Save';
+    editablePlayerName = <input type="text" value={playerName} onChange={handlePlayerNameChange} required />;
   }
 
   return (
     <li>
-      <span>
-        {playerName}
+      <span className="player">
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
