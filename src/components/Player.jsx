287a8fc6d82e85df1f452892d2377d28654bfffb
Andrew <andrewchau1993@gmail.com>
2024-12-02 13:26:35 -0800

    created gameBoard component, also lifted up state of activePlayer to both GameBoard and Player components to allow for both children usage

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -8,19 +8,18 @@ export default function Player({ initialName, symbol }) {
     setIsEditing((editing) => !editing);
   }
 
-  function handlePlayerNameChange(event) {
-    console.log(event);
+  function handleChange(event) {
     setPlayerName(event.target.value);
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    editablePlayerName = <input type="text" value={playerName} onChange={handlePlayerNameChange} required />;
+    editablePlayerName = <input type="text" value={playerName} onChange={handleChange} required />;
   }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
